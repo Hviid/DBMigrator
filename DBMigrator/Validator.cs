adc7b98cdc2b708855a2d55190aa20bfb0b48973
Martin Andersen <ma@Judex.DK>
2016-09-08 13:32:18 +0200

    Removed unnecessary $ signs in string interpolation

--- DBMigrator/Validator.cs
+++ DBMigrator/Validator.cs
@@ -14,17 +14,17 @@ namespace DBMigrator
             foreach (var targetVersion in target)
             {
                 var sourceVersion = source.SingleOrDefault(t => t.Name == targetVersion.Name);
-                if (sourceVersion == null) throw new Exception($"Could not find target version ${targetVersion.Name} in source");
+                if (sourceVersion == null) throw new Exception($"Could not find target version {targetVersion.Name} in source");
 
                 foreach (var targetFeature in targetVersion.Features)
                 {
                     var sourceFeature = sourceVersion.Features.SingleOrDefault(s => s.Name == targetFeature.Name);
-                    if(sourceFeature == null) throw new Exception($"Could not find target feature ${targetFeature.Name} in source for version {targetVersion.Name}");
+                    if(sourceFeature == null) throw new Exception($"Could not find target feature {targetFeature.Name} in source for version {targetVersion.Name}");
 
                     foreach (var targetScript in targetFeature.UpgradeScripts)
                     {
                         var sourceScript = sourceFeature.UpgradeScripts.SingleOrDefault(s => s.FileName == targetScript.FileName);
-                        if (sourceScript == null) throw new Exception($"Could not find target script {targetScript.FileName} in target feature ${targetFeature.Name} in source for version {targetVersion.Name}");
+                        if (sourceScript == null) throw new Exception($"Could not find target script {targetScript.FileName} in target feature {targetFeature.Name} in source for version {targetVersion.Name}");
                         if (sourceScript.Order != targetScript.Order) throw new Exception($"Target script {targetScript.FileName} order {targetScript.Order} are not equal to source script {sourceScript.FileName} order {sourceScript.Order}");
                         if(sourceScript.Checksum != targetScript.Checksum) throw new Exception($"Target script {targetScript.FileName} checksum {targetScript.Checksum} are not equal to source script {sourceScript.FileName} checksum {sourceScript.Checksum}");
                     }

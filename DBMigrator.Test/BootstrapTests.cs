f725fd3436e4b7442168d69a6d63a0bbcdb9b53d
Martin Andersen <ma@Judex.DK>
2016-09-15 21:46:59 +0200

    Added bootstraping of DI for unittest assembly

---
+++ DBMigrator.Test/BootstrapTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBMigrator.Test
+{
+    [TestClass]
+    public class BootstrapTests
+    {
+        [AssemblyInitialize]
+        public static void Bootstrap(TestContext context)
+        {
+            Bootstrapper.ConfigureServices(new ServiceCollection());
+        }
+    }
+}

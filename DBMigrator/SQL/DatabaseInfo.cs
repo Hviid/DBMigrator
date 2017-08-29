0956614bb99a47621eb2f20196cd47b452781672
Martin Andersen <ma@Judex.DK>
2017-08-29 15:01:55 +0200

    Added SQL for getting changed objects

---
+++ DBMigrator/SQL/DatabaseInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBMigrator.SQL
+{
+    static class DatabaseInfo
+    {
+        static string GetObjectsChangedSince(DateTime dateTime)
+        {
+            return "SELECT [name] " +
+                ",[object_id] " +
+                ",[principal_id] " +
+                ",[schema_id] " +
+                ",[parent_object_id] " +
+                ",[type] " +
+                ",[type_desc] " +
+                ",[create_date] " +
+                ",[modify_date] " +
+                ",[is_ms_shipped] " +
+                ",[is_published] ,[is_schema_published] " +
+                "FROM [frapp_dev].[sys].[objects] " +
+                "WHERE is_ms_shipped = 0 " +
+                $"AND modify_date > {dateTime.ToString()}";
+        }
+    }
+}

11dd9e9fcd1cd302572c9ebc182f1440e4b79dba
Martin Andersen <martin.hviid.andersen@gmail.dk>
2019-09-24 20:54:05 +0200

    Added DBMigrator Visual Studio extension

---
+++ DBMigratorVsix/DBMigratorWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace DBMigratorVsix
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("59f61265-32f1-42d8-830b-070a752e8bea")]
+    public class DBMigratorWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DBMigratorWindow"/> class.
+        /// </summary>
+        public DBMigratorWindow() : base(null)
+        {
+            this.Caption = "DBMigratorWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new DBMigratorWindowControl();
+        }
+
+        internal void SetMigrationPath(string folderPath)
+        {
+
+            ((DBMigratorWindowControl)this.Content).SetMigrationPath(folderPath);
+        }
+    }
+}
---
+++ DBMigratorVsix/DBMigratorWindowControl.xaml.cs
@@ -0,0 +1,85 @@
+﻿namespace DBMigratorVsix
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DBMigratorWindowControl.
+    /// </summary>
+    public partial class DBMigratorWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DBMigratorWindowControl"/> class.
+        /// </summary>
+        public DBMigratorWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        class Data
+        {
+            public string MigrationsPath { get; set; } = "";
+            public string TargetVersion { get; set; } = "";
+        }
+        private void Upgrade(object sender, RoutedEventArgs e)
+        {
+            var username = Username.Text;
+            var password = Password.Password;
+            var server = Server.Text;
+            var database = Database.Text;
+            var path = MigrationPath.Text;
+
+            var cmd = $@"C:\dbmigrator\dbmigrator.exe upgrade -s {server} -d {database}";
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                cmd += $" -u {username} -p {password}";
+
+            cmd += $"-f {path}";
+
+            System.Diagnostics.Process.Start("CMD.exe", $"/k {cmd}");
+        }
+
+        private void Downgrade(object sender, RoutedEventArgs e)
+        {
+            var username = Username.Text;
+            var password = Password.Password;
+            var server = Server.Text;
+            var database = Database.Text;
+            var path = MigrationPath.Text;
+
+            var cmd = $@"C:\dbmigrator\dbmigrator.exe downgrade -s {server} -d {database}";
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                cmd += $" -u {username} -p {password}";
+
+            cmd += $"-f {path}";
+
+            System.Diagnostics.Process.Start("CMD.exe", $"/k {cmd}");
+        }
+
+        internal void SetMigrationPath(string folderPath)
+        {
+            var targetVersion = "";
+
+            try
+            {
+                var dirName = new DirectoryInfo(folderPath);
+                var version = new Version(dirName.Name);
+                targetVersion = dirName.Name;
+                folderPath = dirName.Parent.FullName;
+            }
+            catch (Exception)
+            {
+            }
+
+            this.DataContext = new Data
+            {
+                MigrationsPath = folderPath,
+                TargetVersion = targetVersion
+            };
+        }
+    }
+}
